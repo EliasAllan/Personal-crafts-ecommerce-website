6df084818b5b689ac59e9731002ed86b70282f1f
AllanEl <allanrnelias@gmail.com>
2023-06-02 00:13:33 -0400

    added to javascript

--- client/test.js
+++ client/test.js
@@ -1,5 +1,17 @@
-const timeline = document.querySelectorAll('.timeline li .data')
+const timeline_wrapper = document.querySelector('.timeline-wrapper'),
+timelines = document.querySelectorAll('.timeline li .data');
 
-for(const time of timeline){
+for(const time of timelines){
     time.onclick = () => time.classList.toggle('show')
 }
+
+timeline_wrapper.addEventListener('mousemove', (event) =>{
+    const timeline = document.querySelector('.timeline');
+    let scroll_width = event.pageX / timeline_wrapper.clientWidth 
+             * (timeline.clientWidth - timeline_wrapper.clientWidth)
+    console.log({
+        'timeline_width' : timeline.clientWidth,
+        'timeline_wrapper_width' : timeline_wrapper.clientWidth,
+        'Mouse X-codinate' : event.pageX
+    })
+})
